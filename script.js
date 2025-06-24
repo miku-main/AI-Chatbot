fd122365525cd4d53eed43d19f7b6edd85b3ca0a
Gui Sen Zou <125292379+miku-main@users.noreply.github.com>
2025-06-23 20:50:24 -0400

    script.js fix again

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function addAnswerSection(question) {
     const answerSection = document.createElement('section');
     answerSection.classList.add('answer-section');
     answerSection.id = `answer-${answerSectionId}`;
+    answerSection.innerHTML = getLoadingSvg();
     content.appendChild(answerSection);
 
     isAnswerLoading = true;
@@ -53,12 +54,14 @@ function getAnswer(question, id) {
     .then(data => {
         const answerEl = document.getElementById(`answer-${id}`);
         if (answerEl) {
-          answerEl.innerText = data.reply;
+            answerEl.innerText = data.reply;
         }
-        })
+    })
     .catch(err => {
         const errorEl = document.getElementById(`answer-${id}`);
-        if (errorEl) errorEl.innerText = "Error getting response.";
+        if (errorEl) {
+            errorEl.innerText = "Error getting response.";
+        }
     })
     .finally(() => {
         isAnswerLoading = false;
@@ -66,40 +69,17 @@ function getAnswer(question, id) {
     });
 }
 
-function addQuestionSection(message) {
-    isAnswerLoading = true;
-    // Create section element
-    const sectionElement = document.createElement('section');
-    sectionElement.className = 'question-section';
-    sectionElement.textContent = message;
-
-    content.appendChild(sectionElement);
-    // Add answer section after added question section
-    addAnswerSection(message)
-    scrollToBottom();
-}
-
-function addAnswerSection(message) {
-    if (isAnswerLoading) {
-        // Increment answer section ID for tracking
-        answerSectionId++;
-        // Create and empty answer section with a loading animation
-        const sectionElement = document.createElement('section');
-        sectionElement.className = 'answer-section';
-        sectionElement.innerHTML = getLoadingSvg();
-        sectionElement.id = `answer-${answerSectionId}`;
-
-        content.appendChild(sectionElement);
-        getAnswer(message, answerSectionId);
-    } else {
-        // Fill in the answer once it's received
-        const answerSectionElement = document.getElementById(answerSectionId);
-        answerSectionElement.textContent = message;
-    }
-}
-
 function getLoadingSvg() {
-    return `<svg style="height: 25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="#137A7F" stroke="#137A7F" stroke-width="15" r="15" cx="40" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></circle><circle fill="#137A7F" stroke="#137A7F" stroke-width="15" r="15" cx="100" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></circle><circle fill="#137A7F" stroke="#137A7F" stroke-width="15" r="15" cx="160" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></circle></svg>`;
+    return `<svg style="height: 25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+<circle fill="#137A7F" stroke="#137A7F" stroke-width="15" r="15" cx="40" cy="65">
+<animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" 
+keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></circle>
+<circle fill="#137A7F" stroke="#137A7F" stroke-width="15" r="15" cx="100" cy="65">
+<animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" 
+keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></circle>
+<circle fill="#137A7F" stroke="#137A7F" stroke-width="15" r="15" cx="160" cy="65">
+<animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" 
+keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></circle></svg>`;
 }
 
 function scrollToBottom() {
